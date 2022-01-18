ec79c6458d2e1118eedfc9e94097bef8aeb7f2ce
Legend <me@liux.pro>
2022-01-18 23:11:22 +0800

    算了，换ui框架吧，element太坑了

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,118 @@
 <template>
-  <div>
-    <el-row>
-      <el-col :md="4" :lg="4" :xl="4" class="hidden-sm-and-down main-menu">
-        <Menu />
-      </el-col>
-    </el-row>
-    <el-row>
-      <!--      https://blog.csdn.net/qq_41813208/article/details/108425529-->
-      <!--      https://blog.csdn.net/sylfffffffff/article/details/105100386-->
-
-      <el-col :md="4" :lg="4" :xl="4" class="hidden-sm-and-down">
-        &nbsp;
-      </el-col>
-      <el-col :xs="24" :sm="24" :md="20" :lg="16" :xl="16">
-        <nuxt-child />
-      </el-col>
-      <el-col :lg="4" :xl="4" class="hidden-md-and-down">
-        &nbsp;
-      </el-col>
-      <!--      <el-col :xs="0" :sm="0" :md="2" :lg="3" :xl="3"></el-col>-->
-    </el-row>
-  </div>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import 'element-ui/lib/theme-chalk/display.css'
-// import Header from '@/components/Header'
-
 export default {
   name: 'DefaultLayout',
-  // components: { Header },
   data () {
-    return {}
-  },
-  methods: {
-    goBack () {
-      console.log('go back')
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
     }
   }
 }
 </script>
-<style>
-.main-menu {
-  position: fixed;
-  z-index: 9999;
-}
-body {
-  margin: 0;
-}
-</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    titleTemplate: '%s - liux.pro',
     title: 'liux.pro',
-    htmlAttrs: {
-      lang: 'en'
-    },
     meta: [
       { charset: 'utf-8' },
       {
@@ -31,14 +31,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -46,24 +42,18 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content'
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa'
   ],
-
-  hooks: {
-    // 用插件强制打开文件修改监听
-    // 开发模式会自动开文件监听，开发模式给他强制打开
-    'content:ready': ($content) => {
-      $content.database.watch()
-    }
-  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
@@ -71,14 +61,34 @@ export default {
     baseURL: '/'
   },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {
-    liveEdit: false
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
   },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    transpile: [/^element-ui/]
-  },
+  build: {},
   ssr: true, // Disable Server Side rendering
   target: 'server'
 }
