93bda5f6af6872f92f0b4cf74636333064a37070
Legend <me@liux.pro>
2022-01-20 03:12:45 +0800

    milkdown api设计让我看不懂

--- nuxt.config.js
+++ nuxt.config.js
@@ -91,5 +91,5 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   ssr: false, // Disable Server Side rendering
-  target: 'server'
+  target: 'static'
 }
